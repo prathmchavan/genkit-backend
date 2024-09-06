ef69a99cd3d932289b7188087281235d8d512277
Prathmesh Chavan <96901316+prathmchavan@users.noreply.github.com>
2024-09-06 17:52:02 +0530

    cors updated

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Application } from "express";
 import dotenv from "dotenv";
-import cors from "cors";  
+import cors from "cors";
 import router from "./routes";
 import { connectDB } from "@microproj/mongodb-connector";
 
@@ -14,7 +14,9 @@ app.use(cors({
     origin: ['http://localhost:3001',
         'http://localhost:3000',
         'https://learnium.coolify.top',
-        'https://learnium.coolify.top/ai/apti'],
+        'https://learnium.coolify.top/ai/apti',
+        "https://api.learnium.coolify.top/reel"
+    ],
     methods: ["GET", "HEAD", "OPTIONS", "POST", "PUT", "DELETE"],
     allowedHeaders: 'Content-Type',
 }));
